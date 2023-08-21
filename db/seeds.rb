ed39b32edf58f5adf2b83f779625d66391fea9ec
support <support@kmcintosh-6782.remax.local>
2023-08-21 12:22:23 -0600

    updated seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-Recipient.destroy_all
+MessageRecipient.destroy_all
 Message.destroy_all
+Recipient.destroy_all
+User.destroy_all
 
 # Create 10 fake users
 10.times do
-    User.create(
-      username: Faker::Internet.username,
-      password_digest: BCrypt::Password.create('password123'), # Assuming you are using bcrypt for hashing
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65)
-    )
-  end
+  User.create(
+    username: Faker::Internet.username,
+    password_digest: BCrypt::Password.create('password123'), # Assuming you are using bcrypt for hashing
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65)
+  )
+end
+
+# Create 20 fake recipients
+20.times do
+  Recipient.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
+
+# Create 30 fake messages
+30.times do
+  message = Message.create(
+    subject: Faker::Lorem.sentence(word_count: 3),
+    body: Faker::Lorem.paragraph(sentence_count: 5),
+    user: User.order(Arel.sql('RANDOM()')).first
+  )
   
-  # Create 20 fake recipients
-  20.times do
-    Recipient.create(
-      name: Faker::Name.name,
-      email: Faker::Internet.email
-    )
-  end
-  
-  # Create 30 fake messages
-  30.times do
-    Message.create(
-      subject: Faker::Lorem.sentence(word_count: 3),
-      body: Faker::Lorem.paragraph(sentence_count: 5),
-      user: User.order(Arel.sql('RANDOM()')).first,
-      recipient: Recipient.order(Arel.sql('RANDOM()')).first
-    )
-  end
-  
+  # Create a MessageRecipient for each message
+  MessageRecipient.create(
+    message: message,
+    recipient: Recipient.order(Arel.sql('RANDOM()')).first
+  )
+end
